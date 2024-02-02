4c5dd69f44e5b7a37b3c5d72533e6418c4ece9b1
Jacobw1oo <131918522+Jacobw1oo@users.noreply.github.com>
2024-02-02 13:46:30 -0600

    Update jakebox.sh

--- jakebox.sh
+++ jakebox.sh
@@ -7,7 +7,14 @@
 
 #Todo make a pick menu
 
+
+echo "Installing JakeBox "
+echo "estimated time to completion is 20 minuties"
+echo "-------------------------------------------"
+
 #Part 1 - Termux - Installing proot-debian
+echo "Part 1 - Termux - Installing proot-debian"
+
 echo "allow-external-apps = true" >> ~/.termux/termux.properties && echo "hide-soft-keyboard-on-startup = true" >> ~/.termux/termux.properties
 pkg clean && termux-setup-storage && yes | pkg update && pkg install nano wget proot-distro pulseaudio -y && pkg clean && proot-distro install debian && proot-distro clear-cache &&
 echo 'pulseaudio --verbose --start --exit-idle-time=-1 --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1"
@@ -16,14 +23,19 @@ alias jakeboxsys="proot-distro login debian --user gamer --shared-tmp --no-sysvi
 alias jakebox="proot-distro login debian --user gamer --shared-tmp -- bash -c "termux-x11 :0 -xstartup "dbus-launch --exit-with-session xfce4-session"
 ""' >> ~/.bashrc && . ~/.bashrc && termux-reload-settings
 
+echo "Part 1 - Done"
+
 #Todo make a pick menu
 
 #Part 2 - Termux - Installing VirGL Zink
+echo "Part 2 - Termux - Installing VirGL Zink"
+
 pkg install x11-repo tur-repo
 pkg install mesa-zink virglrenderer-mesa-zink
 
 echo 'alias zink="MESA_NO_ERROR=1 MESA_GL_VERSION_OVERRIDE=4.3COMPAT MESA_GLES_VERSION_OVERRIDE=3.2 GALLIUM_DRIVER=zink ZINK_DESCRIPTORS=lazy virgl_test_server --use-egl-surfaceless --use-gles &"' >> ~/.bashrc
 source ~/.bashrc
+echo "Part 2 - Done"
 
 #Part 2.1 - Termux - Installing  VirGL ES - Disabled
 #pkg install x11-repo
@@ -33,6 +45,7 @@ source ~/.bashrc
 #source ~/.bashrc
 
 #Part Pre 3 - Termux - prep for xfce4
+echo "Part 3 prep - Termux - prep for xfce4 & loading into proot"
 pkg update
 pkg install x11-repo -Y
 pkg install termux-x11-nightly -Y
@@ -42,5 +55,6 @@ pkg install xfce -Y
 #loads into proot and continues the script at jakeboxproot
 proot-distro login debian --user root --shared-tmp --no-sysvipc -- bash -c "curl -s -o ~/jakeboxproot.sh https://raw.githubusercontent.com/Jacobw1oo/JakeBox/main/jakeboxproot.sh; bash .jakeboxproot.sh"
 
+echo "Part 3 prep - Done"
 #Username: gamer Password: gamer
 
