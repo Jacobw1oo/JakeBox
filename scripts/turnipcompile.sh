1199d6221f6b3a54ce673c3ae64a24b8ad70211d
Jacobw1oo <131918522+Jacobw1oo@users.noreply.github.com>
2024-02-02 02:46:38 -0600

    Update turnipcompile.sh

--- scripts/turnipcompile.sh
+++ scripts/turnipcompile.sh
@@ -34,7 +34,7 @@ MESA_PREFIX=${BUILD_PREFIX}/mesa
 MESA_VER=$(cat ${MESA_PREFIX}/VERSION)
 DATE=$(date +"%F" | sed 's/-//g')
 MESA_64=${BUILD_PREFIX}/mesa-vulkan-kgsl_${MESA_VER}-${DATE}_arm64
-MESA_86=${BUILD_PREFIX}/mesa-vulkan-kgsl_${MESA_VER}-${DATE}_arm86
+MESA_32=${BUILD_PREFIX}/mesa-vulkan-kgsl_${MESA_VER}-${DATE}_arm32
 
 #applying patch
 cd ${MESA_PREFIX}/src/
@@ -44,13 +44,13 @@ git apply -v wsi-termux-x11-v3.patch
 
 #Building arm64 binary
 # all arm64 requirments
-sudo apt install zlib1g-dev:arm64 libexpat1-dev:arm64 libdrm-dev:arm64 libx11-dev:arm64 libx11-xcb-dev:arm64 libxext-dev:arm64 libxdamage-dev:arm64 libxcb-glx0-dev:arm64 libxcb-dri2-0-dev:arm64 libxcb-dri3-dev:arm64 libxcb-shm0-dev:arm64 libxcb-present-dev:arm64 libxshmfence-dev:arm64 libxxf86vm-dev:arm64 libxrandr-dev:arm64 libwayland-dev:arm64 wayland-protocols:arm64 libwayland-egl-backend-dev:arm64
+sudo apt install zlib1g-dev:arm64 libexpat1-dev:arm64 libdrm-dev:arm64 libx11-dev:arm64 libx11-xcb-dev:arm64 libxext-dev:arm64 libxdamage-dev:arm64 libxcb-glx0-dev:arm64 libxcb-dri2-0-dev:arm64 libxcb-dri3-dev:arm64 libxcb-shm0-dev:arm64 libxcb-present-dev:arm64 libxshmfence-dev:arm64 libxxf32vm-dev:arm64 libxrandr-dev:arm64 libwayland-dev:arm64 wayland-protocols:arm64 libwayland-egl-backend-dev:arm64
 #apt-utils might be needed?
 
 cd ${MESA_PREFIX}
 mkdir build64
 
-# file to handle host build config, we have two due to cross compile for arm86 (armhf)
+# file to handle host build config, we have two due to cross compile for arm32 (armhf)
 echo "[binaries]
 c = 'arm-linux-gnueabihf-gcc'
 cpp = 'arm-linux-gnueabihf-g++'
@@ -81,12 +81,12 @@ sudo rm ${MESA_64}/DEBIAN/md5sums ${MESA_64}/DEBIAN/triggers
 sudo rm -rf ${MESA_64}/usr/share/drirc.d
 sudo dpkg-deb --build --root-owner-group ${MESA_64}
 
-#Building arm86 binary
+#Building arm32 binary
 # all armhf requirments
-sudo apt install zlib1g-dev:armhf libexpat1-dev:armhf libdrm-dev:armhf libx11-dev:armhf libx11-xcb-dev:armhf libxext-dev:armhf libxdamage-dev:armhf libxcb-glx0-dev:armhf libxcb-dri2-0-dev:armhf libxcb-dri3-dev:armhf libxcb-shm0-dev:armhf libxcb-present-dev:armhf libxshmfence-dev:armhf libxxf86vm-dev:armhf libxrandr-dev:armhf libwayland-dev:armhf wayland-protocols:armhf libwayland-egl-backend-dev:armhf
+sudo apt install zlib1g-dev:armhf libexpat1-dev:armhf libdrm-dev:armhf libx11-dev:armhf libx11-xcb-dev:armhf libxext-dev:armhf libxdamage-dev:armhf libxcb-glx0-dev:armhf libxcb-dri2-0-dev:armhf libxcb-dri3-dev:armhf libxcb-shm0-dev:armhf libxcb-present-dev:armhf libxshmfence-dev:armhf libxxf32vm-dev:armhf libxrandr-dev:armhf libwayland-dev:armhf wayland-protocols:armhf libwayland-egl-backend-dev:armhf
 
 cd ${MESA_PREFIX}
-mkdir build86
+mkdir build32
 
 echo "[binaries]
 c = 'arm-linux-gnueabihf-gcc'
@@ -98,26 +98,26 @@ pkgconfig = 'arm-linux-gnueabihf-pkg-config'
 [host_machine]
 system = 'linux'
 cpu_family = 'arm'
-cpu = 'armhf'
+cpu = 'armv7l'
 endian = 'little'
-" > ${MESA_PREFIX}/arm86.txt
+" > ${MESA_PREFIX}/arm32.txt
 
-sudo meson build86/ --prefix /usr --libdir lib/aarch64-linux-gnu/ -D platforms=x11,wayland -D gallium-drivers=freedreno -D vulkan-drivers=freedreno -D freedreno-kmds=msm,kgsl -D dri3=enabled -D buildtype=release -D glx=disabled -D egl=disabled -D gles1=disabled -D gles2=disabled -D gallium-xa=disabled -D opengl=false -D shared-glapi=false -D b_lto=true -D b_ndebug=true -D cpp_rtti=false -D gbm=disabled -D llvm=disabled -D shared-llvm=disabled -D xmlconfig=disabled
-sudo meson compile -C build86/
-sudo meson install -C build86/ --destdir ${MESA_86}
+sudo meson build32/ --cross-file arm32.txt --prefix /usr --libdir lib/aarch64-linux-gnu/ -D platforms=x11,wayland -D gallium-drivers=freedreno -D vulkan-drivers=freedreno -D freedreno-kmds=msm,kgsl -D dri3=enabled -D buildtype=release -D glx=disabled -D egl=disabled -D gles1=disabled -D gles2=disabled -D gallium-xa=disabled -D opengl=false -D shared-glapi=false -D b_lto=true -D b_ndebug=true -D cpp_rtti=false -D gbm=disabled -D llvm=disabled -D shared-llvm=disabled -D xmlconfig=disabled
+sudo meson compile -C build32/
+sudo meson install -C build32/ --destdir ${MESA_32}
 
 
-#building arm86 deb
+#building arm32 deb
 cd ${BUILD_PREFIX}
 
 sudo apt remove -y mesa-vulkan-drivers:armhf
 sudo apt download mesa-vulkan-drivers:armhf
-sudo dpkg-deb -e mesa-vulkan-drivers_*_armhf.deb ${MESA_86}/DEBIAN/
-sudo sed -ie "3s/.*/Version: ${MESA_VER}-${DATE}/g" ${MESA_86}/DEBIAN/control
+sudo dpkg-deb -e mesa-vulkan-drivers_*_armhf.deb ${MESA_32}/DEBIAN/
+sudo sed -ie "3s/.*/Version: ${MESA_VER}-${DATE}/g" ${MESA_32}/DEBIAN/control
 sudo rm mesa-vulkan-drivers_*_armhf.deb
-sudo rm ${MESA_86}/DEBIAN/md5sums ${MESA_86}/DEBIAN/triggers
-sudo rm -rf ${MESA_86}/usr/share/drirc.d
-sudo dpkg-deb --build --root-owner-group ${MESA_86}
+sudo rm ${MESA_32}/DEBIAN/md5sums ${MESA_32}/DEBIAN/triggers
+sudo rm -rf ${MESA_32}/usr/share/drirc.d
+sudo dpkg-deb --build --root-owner-group ${MESA_32}
 
 
 
