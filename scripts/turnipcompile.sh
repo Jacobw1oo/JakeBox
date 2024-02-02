b9f4bca40db0b94632ab3588d3040ef816876814
Jacobw1oo <131918522+Jacobw1oo@users.noreply.github.com>
2024-02-02 01:49:28 -0600

    Update turnipcompile.sh

--- scripts/turnipcompile.sh
+++ scripts/turnipcompile.sh
@@ -3,15 +3,27 @@
 #turnipcompile.sh
 
 #some fun file links
+#https://gitlab.freedesktop.org/mesa/mesa
 #https://gitlab.freedesktop.org/Danil/mesa/-/archive/turnip/feature/a7xx-basic-support/mesa-turnip-feature-a7xx-basic-support.tar.gz
 #https://gitlab.freedesktop.org/Danil/mesa/-/tree/feature/turnip/a750
 
 set -e
 
-#Going to try to build the turnip a750 for the 740
-git clone https://gitlab.freedesktop.org/Danil/mesa/-/tree/feature/turnip/a750 /home/gamer
+#getting build-dependencies for Mesa source package
+sudo echo "deb-src [signed-by="/usr/share/keyrings/debian-archive-keyring.gpg"] http://deb.debian.org/debian bookworm main contrib" >> /etc/apt/sources.list
+sudo echo "deb-src [signed-by="/usr/share/keyrings/debian-archive-keyring.gpg"] http://deb.debian.org/debian bookworm-updates main contrib" >> /etc/apt/sources.list
+sudo apt update
+sudo apt-get build-dep mesa
+sudo apt install wget
 
-mkdir build
-cd build
-meson ..
-sudo ninja install
+# main mesa repo
+git clone https://gitlab.freedesktop.org/mesa/mesa /home/gamer/Downloads
+
+
+
+
+
+
+
+#If i had to build mesa for the a750
+#git clone https://gitlab.freedesktop.org/Danil/mesa/-/tree/feature/turnip/a750 /home/gamer
