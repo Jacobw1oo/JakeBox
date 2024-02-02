740b90e3a67dac7ce8ccd59823fdb8451d49e8f8
Jacobw1oo <131918522+Jacobw1oo@users.noreply.github.com>
2024-02-01 23:41:25 -0600

    Create box8664snapdragon.sh

---
+++ scripts/box8664snapdragon.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#Part 5 - Debian User - Compiling and Installing box86-box64 for snapdragon
+#box8664snapdragon.sh
+
+#This script is for Snapdragon devices only.
+#https://github.com/ptitSeb/box86/blob/master/docs/COMPILE.md
+set -e
+
+#Installing build requiremnts
+sudo apt install gcc-arm-linux-gnueabihf make cmake git wget -y
+
+# Box86 clone, compile, and install (-DSD888=1 is for latest Snapdragon processor alt: -DSD845=1)
+git clone https://github.com/ptitSeb/box86 /home/gamer/Downloads/
+cd ~/Downloads/box86; mkdir build; cd build; cmake ../ -DSD888=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo; make -j$(nproc); sudo make install
+
+# Box64 clone, compile, and install (-DSD888=1 is for latest Snapdragon processor alt: -DSD845=1)
+git clone https://github.com/ptitSeb/box64 /home/gamer/Downloads/
+cd ~/Downloads/box64; mkdir build; cd build; cmake ../ -DSD888=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo; make -j$(nproc); sudo make install
+
+cd ~/
