eb74d75ae4353c4febf52da0bf198fabcf5c0c3f
Jacobw1oo <131918522+Jacobw1oo@users.noreply.github.com>
2024-02-02 00:19:31 -0600

    Update box8664compile.sh

--- scripts/box8664compile.sh
+++ scripts/box8664compile.sh
@@ -2,3 +2,11 @@
 #Part 5 - Debian User - Compiling and Installing box86-box64
 #bash box8664compile.sh
 set -e
+
+
+#Grabing box86 and 64 github due to not having binfmt, we use box86/64 bash
+git clone https://github.com/ptitSeb/box86 /home/gamer/
+git clone https://github.com/ptitSeb/box64 /home/gamer/
+# use to fix not having binfmt
+"export BOX86_BASH=~/box86/tests/bash" >> ~/.bashrc
+"export BOX64_BASH=~/box64/tests/bash" >> ~/.bashrc
