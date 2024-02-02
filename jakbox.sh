e29c714e0a57edc758596f3f5e42ee07a868f020
Jacobw1oo <131918522+Jacobw1oo@users.noreply.github.com>
2024-02-01 22:38:29 -0600

    Update jakbox.sh

--- jakbox.sh
+++ jakbox.sh
@@ -5,12 +5,15 @@
 # https://github.com/cheadrian/termux-chroot-proot-wine-box86_64/
 # https://github.com/Raezroth/Linux-ARM-Gaming-Chroot
 
+#Todo make a pick menu
 
 #Part 1 - Termux - Installing proot-debian
 echo "allow-external-apps = true" >> ~/.termux/termux.properties && echo "hide-soft-keyboard-on-startup = true" >> ~/.termux/termux.properties
 pkg clean && termux-setup-storage && yes | pkg update && pkg install nano wget proot-distro pulseaudio -y && pkg clean && proot-distro install debian && proot-distro clear-cache &&
 echo 'pulseaudio --verbose --start --exit-idle-time=-1 --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1"
 alias jakboxsys="proot-distro login debian --user gamer --shared-tmp --no-sysvipc"' >> ~/.bashrc alias jakbox="proot-distro login debian --user gamer --shared-tmp "' >> ~/.bashrc && . ~/.bashrc && termux-reload-settings
+
+#Todo make a pick menu
 
 #Part 2 - Termux - Installing VirGL Zink
 pkg install x11-repo tur-repo
@@ -20,17 +23,17 @@ echo 'alias zink="MESA_NO_ERROR=1 MESA_GL_VERSION_OVERRIDE=4.3COMPAT MESA_GLES_V
 source ~/.bashrc
 
 #Part 2.1 - Termux - Installing  VirGL ES - Disabled
-#Todo make a pick menu
-
 #pkg install x11-repo
 #pkg install virglrenderer-android
 
 #echo 'alias gl="MESA_NO_ERROR=1 MESA_GL_VERSION_OVERRIDE=4.3COMPAT MESA_GLES_VERSION_OVERRIDE=3.2 virgl_test_server_android &"' >> ~/.bashrc
 #source ~/.bashrc
 
+#Part Pre 3 - Termux - Loading into proot
+
 #Part 3 - Debian - Installing xfce4
-bash scripts/xfce4.sh
+#bash scripts/xfce4.sh
 
 #Part 4 - Debian - Adding User
 #Username: gamer Password: gamer
-bash scripts/addusergamer.sh
+#bash scripts/addusergamer.sh
