b9d22d8f69758362260a66426494da25bacd57ac
Jacobw1oo <131918522+Jacobw1oo@users.noreply.github.com>
2024-02-02 00:16:30 -0600

    Update box8664snapdragon.sh

--- scripts/box8664snapdragon.sh
+++ scripts/box8664snapdragon.sh
@@ -10,12 +10,12 @@ set -e
 sudo apt install gcc-arm-linux-gnueabihf make cmake git wget -y
 
 # Box86 clone, compile, and install (-DSD888=1 is for latest Snapdragon processor alt: -DSD845=1)
-git clone https://github.com/ptitSeb/box86 /home/gamer/Downloads/
-cd ~/Downloads/box86; mkdir build; cd build; cmake ../ -DSD888=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo; make -j$(nproc); sudo make install
+git clone https://github.com/ptitSeb/box86 /home/gamer/
+cd ~/box86; mkdir build; cd build; cmake ../ -DSD888=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo; make -j$(nproc); sudo make install
 
 # Box64 clone, compile, and install (-DSD888=1 is for latest Snapdragon processor alt: -DSD845=1)
-git clone https://github.com/ptitSeb/box64 /home/gamer/Downloads/
-cd ~/Downloads/box64; mkdir build; cd build; cmake ../ -DSD888=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo; make -j$(nproc); sudo make install
+git clone https://github.com/ptitSeb/box64 /home/gamer/
+cd ~/box64; mkdir build; cd build; cmake ../ -DSD888=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo; make -j$(nproc); sudo make install
 
 cd ~/
 
