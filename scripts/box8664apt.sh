199f9614b7f734bf139ad5aba13352d4d546971e
Jacobw1oo <131918522+Jacobw1oo@users.noreply.github.com>
2024-02-02 00:13:32 -0600

    Update box8664apt.sh

--- scripts/box8664apt.sh
+++ scripts/box8664apt.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 #Part 5 - Debian User - Installing box86-box64 from debian repo
 #bash box8664apt.sh
+
+wget https://ryanfortner.github.io/box86-debs/box86.list -O /etc/apt/sources.list.d/box86.list && wget -qO- https://ryanfortner.github.io/box86-debs/KEY.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/box86-debs-archive-keyring.gpg
+wget https://ryanfortner.github.io/box64-debs/box64.list -O /etc/apt/sources.list.d/box64.list && wget -qO- https://ryanfortner.github.io/box64-debs/KEY.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/box64-debs-archive-keyring.gpg
+sudo apt update
+sudo apt install box86-android box64-android
