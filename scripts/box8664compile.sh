8dee8fa590378e329c3dc0aacfe5f429ae6030c5
Jacobw1oo <131918522+Jacobw1oo@users.noreply.github.com>
2024-02-02 02:19:10 -0600

    Update box8664compile.sh

--- scripts/box8664compile.sh
+++ scripts/box8664compile.sh
@@ -6,11 +6,11 @@ set -e
 sudo apt install gcc-arm-linux-gnueabihf make cmake git wget
 
 # Box86 clone, compile, and install (-DRK3399=1 is for most AArch64)
-git clone https://github.com/ptitSeb/box86 /home/gamer/
+git clone https://github.com/ptitSeb/box86 /home/gamer/box86
 cd ~/box86; mkdir build; cd build; cmake ../ -DRK3399=1; make -j$(nproc); sudo make install
 
 # Box64 clone, compile, and install (-DRK3399=1 is for most AArch64)
-git clone https://github.com/ptitSeb/box64 /home/gamer/
+git clone https://github.com/ptitSeb/box64 /home/gamer/box64
 cd ~/box64; mkdir build; cd build; cmake ../ -DRK3399=1; make -j$(nproc); sudo make install
 
 cd ~/
