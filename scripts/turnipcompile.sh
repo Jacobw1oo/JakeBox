f8c9c700d261708e7bd6487650c3cc1c2d139ad3
Jacobw1oo <131918522+Jacobw1oo@users.noreply.github.com>
2024-02-02 02:35:02 -0600

    Update turnipcompile.sh

--- scripts/turnipcompile.sh
+++ scripts/turnipcompile.sh
@@ -45,6 +45,7 @@ git apply -v wsi-termux-x11-v3.patch
 #Building arm64 binary
 # all arm64 requirments
 sudo apt install zlib1g-dev:arm64 libexpat1-dev:arm64 libdrm-dev:arm64 libx11-dev:arm64 libx11-xcb-dev:arm64 libxext-dev:arm64 libxdamage-dev:arm64 libxcb-glx0-dev:arm64 libxcb-dri2-0-dev:arm64 libxcb-dri3-dev:arm64 libxcb-shm0-dev:arm64 libxcb-present-dev:arm64 libxshmfence-dev:arm64 libxxf86vm-dev:arm64 libxrandr-dev:arm64 libwayland-dev:arm64 wayland-protocols:arm64 libwayland-egl-backend-dev:arm64
+#apt-utils might be needed?
 
 echo "[binaries]
 c = 'arm-linux-gnueabihf-gcc'
