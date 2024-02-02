9ff6d0308be3ffe1a72b9c9210f9514874364709
Jacobw1oo <131918522+Jacobw1oo@users.noreply.github.com>
2024-02-02 00:29:43 -0600

    Update box8664apt.sh

--- scripts/box8664apt.sh
+++ scripts/box8664apt.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Part 5 - Debian User - Installing box86-box64 from debian repo
+#Part 6 - Debian User - Installing box86-box64 from debian repo
 #bash box8664apt.sh
 
 wget https://ryanfortner.github.io/box86-debs/box86.list -O /etc/apt/sources.list.d/box86.list && wget -qO- https://ryanfortner.github.io/box86-debs/KEY.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/box86-debs-archive-keyring.gpg
