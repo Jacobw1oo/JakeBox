7aa0a29b3c65350e2c546e1f4c7b914ff5fe500e
Jacobw1oo <131918522+Jacobw1oo@users.noreply.github.com>
2024-02-01 22:06:35 -0600

    Frist upload

---
+++ scripts/addusergamer.sh
@@ -0,0 +1,24 @@
+#Part 4 - Debian - Adding User
+
+#!/bin/bash
+#Username: gamer Password: gamer
+set -e
+
+# Not sure what these do
+groupadd storage
+groupadd wheel
+groupadd video
+groupadd storage
+
+# Adding user
+useradd -U -m -G wheel,audio,video,storage -s /bin/bash gamer -p gamer
+
+# Making sure sudo is installed
+apt install -y sudo
+# Adding them to admin sudoers file
+echo "box  ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+
+# Update alias command to with user
+alias jakbox="proot-distro login debian --user gamer --shared-tmp --no-sysvipc"' >> ~/.bashrc
+# Update alias command to with user and for steam
+alias jakboxsteam="proot-distro login debian --user gamer --shared-tmp "' >> ~/.bashrc
