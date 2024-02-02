1d1a8b874b1625f65fe3326e74527e61e7318f47
Jacobw1oo <131918522+Jacobw1oo@users.noreply.github.com>
2024-02-01 23:05:06 -0600

    Update addusergamer.sh

--- scripts/addusergamer.sh
+++ scripts/addusergamer.sh
@@ -15,7 +15,7 @@ useradd -U -m -G wheel,audio,video,storage -s /bin/bash gamer -p gamer
 # Making sure sudo is installed
 apt install -y sudo
 # Adding them to admin sudoers file
-echo "box  ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+echo "gamer  ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 
 # Update alias command to with user
 alias jakbox="proot-distro login debian --user gamer --shared-tmp --no-sysvipc"' >> ~/.bashrc
