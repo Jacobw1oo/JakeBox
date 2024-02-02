52687c5fe6da9f4d44c020ce8275a7957235c851
Jacobw1oo <131918522+Jacobw1oo@users.noreply.github.com>
2024-02-02 13:28:10 -0600

    Update turnipmesa24-1.sh

--- scripts/turnipmesa24-1.sh
+++ scripts/turnipmesa24-1.sh
@@ -3,9 +3,12 @@
 #turnipv4.sh
 set -e
 
+#making packages directory
 mkdir /usr/local/bin/jakebox/packages
 
+#Downloading turnip zink drivers
+curl -s -o /usr/local/bin/jakebox/packages/mesa-vulkan-kgsl_24.1.0-devel-20240202_arm64.deb https://raw.githubusercontent.com/Jacobw1oo/jakebox/main/packages/mesa-vulkan-kgsl_24.1.0-devel-20240202_arm64.deb
+curl -s -o /usr/local/bin/jakebox/packages/mesa-vulkan-kgsl_24.1.0-devel-20240202_arm32.deb https://raw.githubusercontent.com/Jacobw1oo/jakebox/main/packages/mesa-vulkan-kgsl_24.1.0-devel-20240202_arm32.deb
 
-curl -s -o /usr/local/bin/jakebox/packages/xfce4.sh https://raw.githubusercontent.com/Jacobw1oo/jakebox/main/scripts/xfce4.sh
-
-curl -s -o /usr/local/bin/jakebox/packages/xfce4.sh https://raw.githubusercontent.com/Jacobw1oo/jakebox/main/scripts/xfce4.sh
+#Instilling Drivers
+sudo dkpg -i /usr/local/bin/jakebox/packages/mesa-vulkan-kgsl_24.1.0-devel-20240202_arm*
