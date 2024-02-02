11fb7f8ab784070bbaedc47a403e46bf8a6a26a8
Jacobw1oo <131918522+Jacobw1oo@users.noreply.github.com>
2024-02-02 13:25:10 -0600

    Update turnipmesa24-1.sh

--- scripts/turnipmesa24-1.sh
+++ scripts/turnipmesa24-1.sh
@@ -6,3 +6,6 @@ set -e
 mkdir /usr/local/bin/jakebox/packages
 
 
+curl -s -o $JAKESCRIPTS/../packages/xfce4.sh https://raw.githubusercontent.com/Jacobw1oo/jakebox/main/scripts/xfce4.sh
+
+curl -s -o $JAKESCRIPTS/xfce4.sh https://raw.githubusercontent.com/Jacobw1oo/jakebox/main/scripts/xfce4.sh
