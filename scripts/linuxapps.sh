9d3aa65e66cdfc60ce2e7a33dcc09e14088ce07f
Jacobw1oo <131918522+Jacobw1oo@users.noreply.github.com>
2024-02-03 00:30:00 -0600

    Update linuxapps.sh

--- scripts/linuxapps.sh
+++ scripts/linuxapps.sh
@@ -3,4 +3,27 @@
 #linuxapps.sh
 
 # installing firefox
-sudo apt install -y firefox-esr 
+sudo apt install -y firefox-esr playonlinux
+
+# cheadrian idea on how to easily make sure when start containers on playonlinux correctly, I like it, 
+echo '#!/bin/bash -i
+export BOX86_NOBANNER=1 BEFORE_WINE=box86
+playonlinux' > /usr/local/bin/playonlinux32
+chmod +x /usr/local/bin/playonlinux32
+
+echo '#!/bin/bash -i
+export BOX64_NOBANNER=1 BEFORE_WINE=box64
+box64 playonlinux' > /usr/local/bin/playonlinux64
+chmod +x /usr/local/bin/playonlinux64
+
+echo '[Desktop Entry]
+Version=1.0
+Name=PlayOnLinux Box
+Comment=Front-end application for the wine
+Type=Application
+Exec=bash -i -c "source ~/.bashrc && BEFORE_WINE=box86 playonlinux %F"
+Icon=playonlinux
+Categories=Utility;Emulator;' > ~/Desktop/PlayOnLinux_box.desktop
+chmod +x ~/Desktop/PlayOnLinux_box.desktop
+cp ~/Desktop/PlayOnLinux_box.desktop /usr/share/applications/
+# end
