f76dfb74b6c15a39f5258d847548ddc67e5f18ed
Jacobw1oo <131918522+Jacobw1oo@users.noreply.github.com>
2024-02-03 00:49:02 -0600

    Update heroiccompile.sh

--- scripts/heroiccompile.sh
+++ scripts/heroiccompile.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+#Part 9 - Debian User - compile and install heroic games launcher
+#steaminstall.sh
 
+#Make sure Git, NodeJS, and Yarn are installed
+
+#Clone the repo 
+git clone https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher.git --recurse-submodules
+cd HeroicGamesLauncher
+
+#compile heroic games launcher into deb for aarch64
+yarn dist:linux # Optionally specify a package to create (eg: deb, pacman, tar.xz, rpm, AppImage); default: AppImage
+
+#install heroic games launcher deb
