d7f32c730f2a213f8ad4314b93e0a334044e1c6d
Jacobw1oo <131918522+Jacobw1oo@users.noreply.github.com>
2024-02-03 00:36:26 -0600

    Update steaminstall.sh

--- scripts/steaminstall.sh
+++ scripts/steaminstall.sh
@@ -2,9 +2,10 @@
 #Part 9 - Debian User - installing steam
 #steaminstall.sh
 
+# Installing dependencies
 apt install -y binutils xterm file zenity pciutils libcairo2 libcairo2:armhf libxdamage1 libxdamage1:armhf libjpeg-dev libjpeg-dev:armhf libselinux1 libselinux1:armhf libgl1-mesa-glx libgl1-mesa-glx:armhf libogg-dev libogg-dev:armhf libvorbis-dev libvorbis-dev:armhf libice6 libice6:armhf libxss1 libxss1:armhf libxtst6 libxtst6:armhf libbz2-dev libbz2-dev:armhf libsm6 libsm6:armhf
 
-echo -e "${GREEN}Install libpng12.${WHITE}"
+#installing libpng12 a is requirment for steam
 mkdir ~/Downloads/libpng
 cd ~/Downloads/libpng
 wget http://launchpadlibrarian.net/377987065/libpng12-0_1.2.54-1ubuntu1.1_armhf.deb
@@ -13,14 +14,9 @@ dpkg -i *.deb
 cd ~
 rm -r ~/Downloads/libpng
 
-echo -e "${GREEN}Install steam using the Box86 script, in 'box' user.${WHITE}"
+#Installing steam with ptitSeb script included with box86
+chmod +x ~/box86/install_steam.sh
+bash ~/box86/install_steam.sh
 
-sudo -H -i -u box bash << EOF
-wget https://raw.githubusercontent.com/ptitSeb/box86/master/install_steam.sh
-chmod +x install_steam.sh
-./install_steam.sh
-EOF
 
-source ~/.bashrc
-echo -e "${GREEN}Run Steam through bash86, using 'box' user.${WHITE}"
-sudo -E -H -u box box86 steam
+
