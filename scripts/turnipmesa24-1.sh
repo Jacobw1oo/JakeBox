fc26456557d3bd3de2b9eeb3f12291696da5f3cd
Jacobw1oo <131918522+Jacobw1oo@users.noreply.github.com>
2024-02-03 05:08:50 -0600

    Update turnipmesa24-1.sh

--- scripts/turnipmesa24-1.sh
+++ scripts/turnipmesa24-1.sh
@@ -4,9 +4,6 @@
 echo "Part 7 - Debian User - Installing mesa 24.1 turnip zink GPU drivers"
 set -e
 
-#making packages directory
-mkdir /usr/local/bin/jakebox/packages
-
 #Downloading turnip zink drivers
 curl -s -o /usr/local/bin/jakebox/packages/mesa-vulkan-kgsl_24.1.0-devel-20240202_arm64.deb https://raw.githubusercontent.com/Jacobw1oo/jakebox/main/packages/mesa-vulkan-kgsl_24.1.0-devel-20240202_arm64.deb
 curl -s -o /usr/local/bin/jakebox/packages/mesa-vulkan-kgsl_24.1.0-devel-20240202_arm32.deb https://raw.githubusercontent.com/Jacobw1oo/jakebox/main/packages/mesa-vulkan-kgsl_24.1.0-devel-20240202_arm32.deb
