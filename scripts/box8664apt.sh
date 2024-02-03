07cb88eceea1bb14ed9170adf2d0632659c9b229
Jacobw1oo <131918522+Jacobw1oo@users.noreply.github.com>
2024-02-03 01:57:01 -0600

    Update box8664apt.sh

--- scripts/box8664apt.sh
+++ scripts/box8664apt.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-#Part 6 - Debian User - Installing box86-box64 from debian repo
 #box8664apt.sh
+#Part 9 - Debian User - Installing box86-box64 from debian repo
+echo "Part 9 - Debian User - Installing box86-box64 from debian repo"
+
 
 wget https://ryanfortner.github.io/box86-debs/box86.list -O /etc/apt/sources.list.d/box86.list && wget -qO- https://ryanfortner.github.io/box86-debs/KEY.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/box86-debs-archive-keyring.gpg
 wget https://ryanfortner.github.io/box64-debs/box64.list -O /etc/apt/sources.list.d/box64.list && wget -qO- https://ryanfortner.github.io/box64-debs/KEY.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/box64-debs-archive-keyring.gpg
@@ -16,3 +18,5 @@ sudo chmod +x ~/box86/tests/bash
 sudo chmod +x ~/box64/tests/bash
 echo "export BOX86_BASH=~/box86/tests/bash
 'export BOX64_BASH=~/box64/tests/bash" >> ~/.bashrc
+
+echo "Part 9 - Done"
