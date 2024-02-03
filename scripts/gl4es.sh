bb2b864ed521a98948a5149092ae090c74d96101
Jacobw1oo <131918522+Jacobw1oo@users.noreply.github.com>
2024-02-02 22:35:10 -0600

    Update gl4es.sh

--- scripts/gl4es.sh
+++ scripts/gl4es.sh
@@ -1,12 +1,14 @@
 #gl4es.sh
 
-#Get dependances
+#get dependances
 apt install -y git gcc build-essential cmake libx11-dev
 
+#git clone
 cd $JAKESCRIPTS/packages/
 git clone https://github.com/ptitSeb/gl4es
 cd gl4es
 
+#compiling and install
 mkdir build; cd build; cmake -S ../../gl4es; make install
 
 cd $JAKESCRIPTS/packages/
