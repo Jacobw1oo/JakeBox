8aea25b718c7d4bb7804254932bfb6c0716aa019
Jacobw1oo <131918522+Jacobw1oo@users.noreply.github.com>
2024-02-03 03:13:35 -0600

    Update jakebox.sh

--- jakebox.sh
+++ jakebox.sh
@@ -65,12 +65,70 @@ pkg install xfce -y
 #Part Pre 3 - Termux - Loading into proot
 #loads into proot and continues the script at jakeboxproot
 echo "Part 3 - Done"
-#proot-distro login debian --user root --shared-tmp --no-sysvipc -- bash -c "curl -s -o ~/jakeboxproot.sh https://raw.githubusercontent.com/Jacobw1oo/JakeBox/main/jakeboxproot.sh; bash jakeboxproot.sh"
+# this starts phase 2
+proot-distro login debian --user root --shared-tmp --no-sysvipc -- bash -c "curl -s -o ~/jakeboxproot.sh https://raw.githubusercontent.com/Jacobw1oo/JakeBox/main/jakeboxproot.sh; bash jakeboxproot.sh"
 
-echo "JakeBox install is complete"
-echo "type jakebox to start the proot"
-echo "then type @@ to start the x11 server"
-echo "-------------------------------------------"
+#start up on Debian - Creating scripts directory
+mkdir /usr/local/bin/jakebox/
+mkdir /usr/local/bin/jakebox/scripts
+export JAKESCRIPTS="/usr/local/bin/jakebox/scripts"
+
+#Part 4 - Debian - Installing xfce4
+curl -s -o $JAKESCRIPTS/xfce4.sh https://raw.githubusercontent.com/Jacobw1oo/jakebox/main/scripts/xfce4.sh
+bash $JAKESCRIPTS/xfce4.sh
+
+#Part 5 - Debian - Adding User
+#Username: gamer Password: gamer
+curl -s -o $JAKESCRIPTS/addusergamer.sh https://raw.githubusercontent.com/Jacobw1oo/jakebox/main/scripts/addusergamer.sh
+bash $JAKESCRIPTS/addusergamer.sh
+su gamer
+# should add id checks here, making sure user is not root user
+
+#Part 6 - Debian User - Installing lots of packages
+curl -s -o $JAKESCRIPTS/debianpackages.sh https://raw.githubusercontent.com/Jacobw1oo/jakebox/main/scripts/debianpackages.sh
+bash $JAKESCRIPTS/debianpackages.sh
+
+#Part 7 - Debian User - Installing mesa turnip zink GPU drivers (having issues hte deb created is not compatable)
+curl -s -o $JAKESCRIPTS/turnipmesa23-3.sh https://raw.githubusercontent.com/Jacobw1oo/jakebox/main/scripts/turnipmesa23-3.sh
+bash $JAKESCRIPTS/turnipmesa23-3.sh
+
+#Part 7 - Debian User - Installing mesa turnip zink GPU drivers (having issues hte deb created is not compatable)
+#curl -s -o $JAKESCRIPTS/turnipmesa24-1.sh https://raw.githubusercontent.com/Jacobw1oo/jakebox/main/scripts/turnipmesa24-1.sh
+#bash $JAKESCRIPTS/turnipmesa24-1.sh
+
+#Part 8 - Debian user - compiling and installing gl4es
+curl -s -o $JAKESCRIPTS/gl4es.sh https://raw.githubusercontent.com/Jacobw1oo/jakebox/main/scripts/gl4es.sh
+bash $JAKESCRIPTS/gl4es.sh
+
+#Part 9 - Debian User - Installing box86-box64 from debian repo
+#curl -s -o $JAKESCRIPTS/box8664apt.sh https://raw.githubusercontent.com/Jacobw1oo/jakebox/main/scripts/box8664apt.sh
+#bash $JAKESCRIPTS/box8664apt.sh
+
+#Part 9 - Debian User - Compiling and Installing box86-box64
+#curl -s -o $JAKESCRIPTS/box8664compile.sh https://raw.githubusercontent.com/Jacobw1oo/jakebox/main/scripts/box8664compile.sh
+#bash $JAKESCRIPTS/box8664compile.sh
+
+#Part 9 - Debian User - Compiling and Installing box86-box64 for snapdragon
+curl -s -o $JAKESCRIPTS/box8664snapdragon.sh https://raw.githubusercontent.com/Jacobw1oo/jakebox/main/scripts/box8664snapdragon.sh
+bash $JAKESCRIPTS/box8664snapdragon.sh
+
+#Part 10 - Debian User - Install the frist Wine
+curl -s -o $JAKESCRIPTS/wine91.sh https://raw.githubusercontent.com/Jacobw1oo/jakebox/main/scripts/wine91.sh
+bash $JAKESCRIPTS/wine91.sh
+
+#Part 11 - Debian User - Installing firefox, playonlinux, Lutris
+curl -s -o $JAKESCRIPTS/linuxapps.sh https://raw.githubusercontent.com/Jacobw1oo/jakebox/main/scripts/linuxapps.sh
+bash $JAKESCRIPTS/linuxapps.sh
+
+#Part 12 - Debian User - Installing steam
+curl -s -o $JAKESCRIPTS/steaminstall.sh https://raw.githubusercontent.com/Jacobw1oo/jakebox/main/scripts/steaminstall.sh
+bash $JAKESCRIPTS/steaminstall.sh
+
+#Part 13 - Debian User - Compiling and Installing Heroic Games Lanucher
+curl -s -o $JAKESCRIPTS/heroiccompile.sh https://raw.githubusercontent.com/Jacobw1oo/jakebox/main/scripts/heroiccompile.sh
+bash $JAKESCRIPTS/heroiccompile.sh
+
+#end
 
 #Username: gamer Password: gamer
 
