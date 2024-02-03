30e8220e0832212d301ca92399be5a0100eaee70
Jacobw1oo <131918522+Jacobw1oo@users.noreply.github.com>
2024-02-03 05:07:23 -0600

    Update turnipmesa23-3.sh

--- scripts/turnipmesa23-3.sh
+++ scripts/turnipmesa23-3.sh
@@ -4,9 +4,6 @@
 echo "Part 7 - Debian User - Installing mesa 23.3 turnip zink GPU drivers"
 set -e
 
-#making packages directory
-mkdir /usr/local/bin/jakebox/packages
-
 #Downloading turnip zink drivers
 curl -s -o /usr/local/bin/jakebox/packages/mesa-vulkan-kgsl_23.3.0-devel-20230812_arm64.deb https://raw.githubusercontent.com/Jacobw1oo/jakebox/main/packages/mesa-vulkan-kgsl_23.3.0-devel-20230812_arm64.deb
 curl -s -o /usr/local/bin/jakebox/packages/mesa-vulkan-kgsl_23.3.0-devel-20230812_arm32.deb https://raw.githubusercontent.com/Jacobw1oo/jakebox/main/packages/mesa-vulkan-kgsl_23.3.0-devel-20230812_arm32.deb
