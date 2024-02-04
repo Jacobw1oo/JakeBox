17f73d351313b3a12f915ca19a0783831147d35f
Jacobw1oo <131918522+Jacobw1oo@users.noreply.github.com>
2024-02-04 15:12:26 -0600

    Update toontownrewirtteninstall.sh

--- scripts/toontownrewirtteninstall.sh
+++ scripts/toontownrewirtteninstall.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
+#gl4es.sh
+#Part 13 - Debian user - compiling and installing gl4es
+echo "Part 13 - Debian user - installing toontown rewritten"
 
-sudo apt install flatpak
 
+sudo apt install -y flatpak
 
 
 https://cdn.toontownrewritten.com/launcher/linux/launcher.flatpakref
