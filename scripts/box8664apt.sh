d40a0d5e8a6ef4a9c712cfd276f70a8030289e01
Jacobw1oo <131918522+Jacobw1oo@users.noreply.github.com>
2024-02-03 19:11:43 -0600

    Update box8664apt.sh

--- scripts/box8664apt.sh
+++ scripts/box8664apt.sh
@@ -3,6 +3,9 @@
 #Part 9 - Debian User - Installing box86-box64 from debian repo
 echo "Part 9 - Debian User - Installing box86-box64 from debian repo"
 
+# If amy command fails, script is stoped
+set -e
+
 wget https://ryanfortner.github.io/box86-debs/box86.list -O /etc/apt/sources.list.d/box86.list && wget -qO- https://ryanfortner.github.io/box86-debs/KEY.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/box86-debs-archive-keyring.gpg
 wget https://ryanfortner.github.io/box64-debs/box64.list -O /etc/apt/sources.list.d/box64.list && wget -qO- https://ryanfortner.github.io/box64-debs/KEY.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/box64-debs-archive-keyring.gpg
 sudo apt update
