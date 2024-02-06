421666de2c414b924fe3f5f393b24e88c7e837ec
Jacobw1oo <131918522+Jacobw1oo@users.noreply.github.com>
2024-02-05 23:19:39 -0600

    Update toontownrewirtteninstall.sh

--- scripts/toontownrewirtteninstall.sh
+++ scripts/toontownrewirtteninstall.sh
@@ -3,14 +3,16 @@
 #Part 13 - Debian user - compiling and installing gl4es
 echo "Part 13 - Debian user - installing toontown rewritten"
 
-
+#Installing flatpak package manager
 sudo apt install -y flatpak
 
-
+#using and fixing flatpak
 sudo dbus-launch flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepohub.flatpak
 sudo dbus-launch flatpak install flathub
 
+#installing tooontown
 cd ~/Downloads
 wget https://cdn.toontownrewritten.com/launcher/linux/launcher.flatpakref
-
+#Installing flatpak for amd64 not (AArch64)
+sudo flatpak install launcher.flatpakref
 #Note you need to use box64 or box86 to start app
