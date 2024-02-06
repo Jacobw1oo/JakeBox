3c7f5e836627b8688df3d74ce4de1ca1337642ba
Jacobw1oo <131918522+Jacobw1oo@users.noreply.github.com>
2024-02-06 01:04:14 -0600

    Update toontownrewirtteninstall.sh

--- scripts/toontownrewirtteninstall.sh
+++ scripts/toontownrewirtteninstall.sh
@@ -6,13 +6,21 @@ echo "Part 13 - Debian user - installing toontown rewritten"
 #Installing flatpak package manager
 sudo apt install -y flatpak
 
+#start dbus session
+dbus-run-session
+sudo dbus-run-session
+
 #using and fixing flatpak
 sudo dbus-launch flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepohub.flatpak
 sudo dbus-launch flatpak install flathub
+
+flatpak remote-ls --all --arch=i386  temp
 
 #installing tooontown
 cd ~/Downloads
 wget https://cdn.toontownrewritten.com/launcher/linux/launcher.flatpakref
 #Installing flatpak for amd64 not (AArch64)
-sudo flatpak install launcher.flatpakref
+sudo dbus-launch flatpak install launcher.flatpakref
+#appling fix to repo
+flatpak remote-ls --all --arch=i386 launcher-origin
 #Note you need to use box64 or box86 to start app
