b4469557bf9f6d944e4ab3969e9a1e2c9b03d6ce
Jacobw1oo <131918522+Jacobw1oo@users.noreply.github.com>
2024-02-09 14:45:44 -0600

    Update nile-compile.sh

--- heroic-dependencies/nile-compile.sh
+++ heroic-dependencies/nile-compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#legendary-compile.sh
-#Debian Proot - compiling legendary
+#nile-compile.sh.sh
+#Debian Proot - compiling nile
 #https://github.com/derrod/legendary
 #command line open source version of epic games
 echo "Debian Proot - compiling legendary"
