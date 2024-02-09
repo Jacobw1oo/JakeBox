8f9073b8eaa04cd7424322d7572e12ff7b28682f
Jacobw1oo <131918522+Jacobw1oo@users.noreply.github.com>
2024-02-09 15:27:31 -0600

    Create gogdl-compile.sh

---
+++ heroic-dependencies/gogdl-compile.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#nile-compile.sh.sh
+#Debian Proot - compiling nile
+#https://github.com/derrod/legendary
+#command line open source version of epic games
+echo "Debian Proot - compiling legendary"
