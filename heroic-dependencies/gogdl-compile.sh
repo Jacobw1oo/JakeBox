2a34bc72f076944bc9edc4a850666421f6eb6791
Jacobw1oo <131918522+Jacobw1oo@users.noreply.github.com>
2024-02-09 15:34:17 -0600

    Update gogdl-compile.sh

--- heroic-dependencies/gogdl-compile.sh
+++ heroic-dependencies/gogdl-compile.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 #nile-compile.sh.sh
 #Debian Proot - compiling nile
-#https://github.com/derrod/legendary
+#https://github.com/asb/gogdl
 #command line open source version of epic games
 echo "Debian Proot - compiling legendary"
+
+sudo apt install lua5.4
+git clone https://github.com/asb/gogdl
+cd gogdl
+
