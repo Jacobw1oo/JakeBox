6a783c6f3172a71bed2bfb58bc99972e4d4e7217
Jacobw1oo <131918522+Jacobw1oo@users.noreply.github.com>
2024-02-09 15:16:44 -0600

    Update nile-compile.sh

--- heroic-dependencies/nile-compile.sh
+++ heroic-dependencies/nile-compile.sh
@@ -4,3 +4,10 @@
 #https://github.com/derrod/legendary
 #command line open source version of epic games
 echo "Debian Proot - compiling legendary"
+
+sudo apt install python3-pycryptodome python3-requests python3-zstandard python3-protobuf python3-json5
+pip install pyinstaller
+git clone https://github.com/imLinguin/nile
+cd nile
+python3 -m venv env
+pyinstaller --onefile --name nile nile/cli.py
