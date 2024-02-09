286d40e6aabdbbbe14cc272119cd5ecded0865cf
Jacobw1oo <131918522+Jacobw1oo@users.noreply.github.com>
2024-02-09 15:25:27 -0600

    Update nile-compile.sh

--- heroic-dependencies/nile-compile.sh
+++ heroic-dependencies/nile-compile.sh
@@ -8,6 +8,8 @@ echo "Debian Proot - compiling legendary"
 sudo apt install python3-pycryptodome python3-requests python3-zstandard python3-protobuf python3-json5
 sudo apt install python3-installer
 sudo apt install python3.11-venv
+pip install wheels
+# pyinstaller is crashing due to not have a precompiled bootloader
 pip install pyinstaller 
 git clone https://github.com/imLinguin/nile
 cd nile
