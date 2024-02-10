c88faf78b2f36944d808fb48ff24cef76d58111e
Jacobw1oo <131918522+Jacobw1oo@users.noreply.github.com>
2024-02-09 19:28:44 -0600

    Update legendary-compile.sh

--- heroic-dependencies/legendary-compile.sh
+++ heroic-dependencies/legendary-compile.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 #legendary-compile.sh
 #Debian Proot - compiling legendary
-#https://github.com/asb/gogdl
-#yet another downloader for GOG.com
-echo "Debian Proot - compiling gogdl"
+#https://github.com/derrod/legendary
+#free and open-source Epic Games Launcher alternative
+echo "Debian Proot - compiling legendary"
 
+
+pip install requests
+git clone https://github.com/Heroic-Games-Launcher/heroic-gogdl
+cd heroic-gogdl
+./bin/gogdl --help
+
+pip install pyinstaller
+pyinstaller --onefile --name gogdl gogdl/cli.py
