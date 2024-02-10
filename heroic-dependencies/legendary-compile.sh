210537fec07494f6e1c950e439cab02d195abd81
Jacobw1oo <131918522+Jacobw1oo@users.noreply.github.com>
2024-02-09 19:26:20 -0600

    Update legendary-compile.sh

--- heroic-dependencies/legendary-compile.sh
+++ heroic-dependencies/legendary-compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#gogdl-compile.sh
-#Debian Proot - compiling gogdl
+#legendary-compile.sh
+#Debian Proot - compiling legendary
 #https://github.com/asb/gogdl
 #yet another downloader for GOG.com
 echo "Debian Proot - compiling gogdl"
