f38b6fa992396f0ec901f7755ccb01e4cd63c7eb
Jacobw1oo <131918522+Jacobw1oo@users.noreply.github.com>
2024-02-09 19:27:26 -0600

    Update gogdl-compile.sh

--- heroic-dependencies/gogdl-compile.sh
+++ heroic-dependencies/gogdl-compile.sh
@@ -5,7 +5,10 @@
 #command line open source version of epic games
 echo "Debian Proot - compiling legendary"
 
-
+pip install requests
 git clone https://github.com/Heroic-Games-Launcher/heroic-gogdl
 cd heroic-gogdl
+./bin/gogdl --help
 
+pip install pyinstaller
+pyinstaller --onefile --name gogdl gogdl/cli.py
