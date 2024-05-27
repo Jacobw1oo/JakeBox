091b4d4a632bcb827a7e9f20fd60974f0dd505a5
Jacobw1oo <131918522+Jacobw1oo@users.noreply.github.com>
2024-05-27 18:54:23 -0500

    Update jakebox.sh

--- jakebox.sh
+++ jakebox.sh
@@ -38,7 +38,7 @@ pkg clean && termux-setup-storage && yes | pkg update && pkg install nano wget p
 
 #adding startup and start commands (had an issue with triple nested quotes )
 echo 'pulseaudio --verbose --start --exit-idle-time=-1 --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1"' >> ~/.bashrc
-echo 'alias jakebox="proot-distro login debian --user gamer --shared-tmp --no-sysvipc" ' >> ~/.bashrc
+echo 'alias jakeboxgamer="proot-distro login debian --user gamer --shared-tmp --no-sysvipc" ' >> ~/.bashrc
 echo 'alias jakeboxroot="proot-distro login debian --user root --shared-tmp --no-sysvipc" ' >> ~/.bashrc
 
 echo proot-distro login debian --user gamer --shared-tmp -- bash -c 'termux-x11 :0 -xstartup "dbus-launch --exit-with-session xfce4-session"' > ~/jakebox
