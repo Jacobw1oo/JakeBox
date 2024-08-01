ad3502e60a9283d88ce4a3c2c58d1516244bdca1
Jacobw1oo <131918522+Jacobw1oo@users.noreply.github.com>
2024-08-01 14:00:18 -0500

    Update part3.sh

--- scripts/part3.sh
+++ scripts/part3.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
 #installing xfce4 desktop (currenlty has known bug that break it)
-apt install xubuntu-desktop
+#apt install xubuntu-desktop
+#apt install xfce4
 
 #installing KDE (Kubuntu) desktop
-#apt install kubuntu-desktop
+apt install kubuntu-desktop
 #Dont install suggested it 3 hour download (instead of 20 mins), it cost 10 extra Gb aswell.
 #apt install kubuntu-desktop --install-suggests
 
 #fixing terminal xfce4 terminal is for xfce4 and konsole is for KDE
-apt install xfce4-terminal
-#apt install konsole
+#apt install xfce4-terminal
+apt install konsole
 
 # change terminal to xfce4 or konsole
 update-alternatives --config x-terminal-emulator
@@ -38,8 +39,54 @@ echo "root ALL=(ALL:ALL) ALL"
 read -p "Press enter to continue"
 nano /etc/sudoers
 
-#login
-su gamer
-cd ~
+#stuff that need to be run at root boot
+echo "update-binfmts --enable" >> ~/.bashrc
+echo "chmod a+rw /dev/fuse" >> ~/.bashrc
 
-#end of part1.sh
+#making sure the audio is loaded
+echo "export PULSE_SERVER=127.0.0.1 " >> ~/.bashrc
+
+#trying to fix render
+#export DISPLAY=$(awk '/nameserver / {print $2; exit}' /etc/resolv.conf 2>/dev/null):0
+
+#installing python 3 and pip
+apt-get install -y python3-pip
+apt-get install -y python3-apport python3-apt python3-aptdaemon python3-aptdaemon.gtk3widgets python3-cairo python3-cffi python3-cffi-backend python3-cryptography python3-cssselect python3-cups python3-cupshelpers python3-dbus python3-dev python3-distupgrade python3-evdev python3-gi python3-gi-cairo python3-ibus-1.0 python3-jinja2 python3-jsonschema python3-launchpadlib python3-lazr.restfulclient python3-lxml python3-mako python3-markupsafe python3-minimal python3-netifaces python3-netplan python3-oauthlib python3-protobuf python3-psutil python3-pyrsistent python3-setproctitle python3-software-properties python3-systemd python3-uno python3-update-manager python3-xcffib
+
+apt remove python3-pil
+python3 -m pip install --break-system-packages pillow
+
+#apt remove python-gi-dev
+#python3 -m pip install --break-system-packages PyGObject
+#apt remove python3-yaml
+#python3 -m pip install --break-system-packages PyYAML
+
+python3 -m pip install meson
+python3 -m pip install ninja
+
+# added a easy startx launcher
+#export DISPLAY=:0
+
+#remove due to causing break (i dont think this is correct)
+#sudo /etc/init.d/dbus start
+
+echo "#!/bin/bash
+sudo /etc/init.d/dbus start
+export DISPLAY=:0
+taskset -c 4-7 startxfce4
+" >  /home/gamer/jakestartx.sh
+chmod 755 /home/gamer/jakestartx.sh
+chmod +x /home/gamer/jakestartx.sh
+
+# expanding system to install a bunch of small things (mostly man docs)
+#unminimize
+
+#fxing shm again
+cd /dev/
+mkdir shm
+chmod 777 shm
+
+#login
+su gamer -c cd ~
+
+#end of part3.sh
