b466932e99b1c808431acdcc06e8923dce9a20f2
Jacobw1oo <131918522+Jacobw1oo@users.noreply.github.com>
2024-09-27 14:13:09 -0500

    Update part3.sh

--- scripts/part3.sh
+++ scripts/part3.sh
@@ -25,6 +25,8 @@ groupadd storage
 groupadd wheel
 #adding user named gamer
 useradd -m -g users -G wheel,audio,video,storage -s /bin/bash gamer
+#adding user to input group
+usermod -a -G input gamer
 #setting password for gamer, password is gamer
 echo "setting password for user (gamer). My password is gamer"
 read -p "Press enter to continue"
@@ -51,24 +53,25 @@ echo "export PULSE_SERVER=127.0.0.1 " >> ~/.bashrc
 
 #installing python 3 and pip
 apt-get install -y python3-pip
+python3 -m pip install meson
+python3 -m pip install ninja
 apt-get install -y python3-apport python3-apt python3-aptdaemon python3-aptdaemon.gtk3widgets python3-cairo python3-cffi python3-cffi-backend python3-cryptography python3-cssselect python3-cups python3-cupshelpers python3-dbus python3-dev python3-distupgrade python3-evdev python3-gi python3-gi-cairo python3-ibus-1.0 python3-jinja2 python3-jsonschema python3-launchpadlib python3-lazr.restfulclient python3-lxml python3-mako python3-markupsafe python3-minimal python3-netifaces python3-netplan python3-oauthlib python3-protobuf python3-psutil python3-pyrsistent python3-setproctitle python3-software-properties python3-systemd python3-uno python3-update-manager python3-xcffib
 
-apt remove python3-pil
-python3 -m pip install --break-system-packages pillow
+#apt remove python3-pil
+#python3 -m pip install --break-system-packages pillow
 
 #apt remove python-gi-dev
 #python3 -m pip install --break-system-packages PyGObject
 #apt remove python3-yaml
 #python3 -m pip install --break-system-packages PyYAML
 
-python3 -m pip install meson
-python3 -m pip install ninja
 
 # added a easy startx launcher
 #export DISPLAY=:0
 
 #remove due to causing break (i dont think this is correct)
 #sudo /etc/init.d/dbus start
+#alt taskset -c 4-7 startplasma-x11
 
 echo "#!/bin/bash
 sudo /etc/init.d/dbus start
@@ -80,6 +83,10 @@ chmod +x /home/gamer/jakestartx.sh
 
 # expanding system to install a bunch of small things (mostly man docs)
 #unminimize
+
+# debuging segment faults
+# gdb xfce4-notifyd-config
+# run
 
 #fxing shm again
 cd /dev/
